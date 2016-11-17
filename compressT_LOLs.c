6be25491035fe7d0252022b1ec924ce151c87d1c
Jessica G <jesseekaaguo@gmail.com>
2016-11-17 14:47:08 -0500

    Add files via upload

---
+++ compressT_LOLs.c
@@ -0,0 +1,25 @@
+/*Jessica Guo (ಠ益ಠщ)
+Christine Do
+
+Assignment02 Threads and Processes */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include "compress.h"
+
+/*read in arguments in the form of:
+compressT_LOLS(<file to compress>, <number of parts>)
+compressR_LOLS(<file to compress>, <number of parts>)
+*/
+void main(int argc, char * argv[]){
+	//check argument input
+	//if compressT 
+		//threads
+	//if compressR
+		//process
+
+
+}
+
