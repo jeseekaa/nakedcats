a7a67a947e0fe9325385b5aa2e0d8d9263e0ac68
Jessica G <jesseekaaguo@gmail.com>
2016-11-22 18:28:47 -0500

    updated
    existence check isnt working correctly

--- compressR_LOLs.c
+++ compressR_LOLs.c
@@ -26,6 +26,9 @@ if(argc != 3) {
 char* fileName = argv[1];
 int numWorkers = atoi(argv[2]);
 
+//char existenceCheck[100];
+//char existenceCheck0[100];
+
 int i = 0;
 pid_t id;
 
@@ -39,6 +42,13 @@ if(stat(fileName, &st)==0){
   printf("file opening error\n");
   exit(1);
 }
+//sprintf(existenceCheck, "%s_LOLS.txt", fileName);
+//sprintf(existenceCheck0, "%s_LOLS0.txt",fileName);
+
+/*if(access(existenceCheck0, F_OK)!= -1 || access(existenceCheck, F_OK != -1)) {
+  printf("file already compressed, try new file\n");
+  exit(1);
+} */
 
 //error for if asking to create more workers than bytes in the files
 //ask for smaller input , exit(1) the program
@@ -48,13 +58,12 @@ if(numWorkers>fileSize){
   exit(1);
 }
 
-//the inputs are acceptanble, exec() the worker file 
-
-char* parameters[] = {"./compressR_worker_LOLs", argv[1], argv[2], NULL, NULL};
+char* parameters[] = {"./worker", argv[1], argv[2], NULL, NULL};
 execvp(parameters[0], parameters);
 printf("ERROR: execvp failed\n");
 
-
+//all the inputs good
+//forking everything and execute the worker file
 return 0;
 
 }
