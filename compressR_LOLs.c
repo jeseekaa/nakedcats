fb1d721d1bb77fb856557edb42465445a7d48d4c
Jessica G <jesseekaaguo@gmail.com>
2016-11-22 17:40:11 -0500

    how to exec()

--- compressR_LOLs.c
+++ compressR_LOLs.c
@@ -8,121 +8,51 @@ Assignment02 Threads and Processes */
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h> //fork() for processes
-#include <pthread.h> //for threads
+//#include <pthread.h> //for threads
 #include <string.h>
-#include <dirent.h> //for readdir()
+#include <sys/stat.h>
 #include <ctype.h>
 #include <time.h>
 
-#define MAX 5000 //500 compressions 
 
-//isspace() for space: spaces, tabs, newline
-//isalpha() for alphabetical
-//isdigit() 
+int main(int argc, char * argv[]){
 
-/*read in arguments in the form of:
-compressT_LOLS(<file to compress>, <number of parts>)
-compressR_LOLS(<file to compress>, <number of parts>)
-*/
+//checking input
+if(argc != 3) {
+    printf("Error: Invalid number of arguments.\n");
+    exit(1);
+  }
 
-//fork() creates processes
-//pthread_create() threads
+char* fileName = argv[1];
+int numWorkers = atoi(argv[2]);
 
+int i = 0;
+pid_t id;
 
-//compress function takes in pointer to beginning and end of string of file
-//char *compress(DIR *dirp, char *start, char *end) are the parameters....
+struct stat st;
+int fileSize;
 
-char *compress(char *input)
-{     
-  int letter_count;
-  char count[MAX];
-  int len = strlen(input);
-   
-  char *output = (char *)malloc(sizeof(char)*(len)+ 1);
-  
-  int i=0; //original string iterator
-  int j = 0; //ouput iterator
-  int k=0; //count iterator
- 
-  //traverse the input 
-  for(i = 0; i < len; i++)
-  {
-    if(isalpha(input[i])){
- 
-    char temp = input[i];
-     
-    // Count the number of occurrences 
-    letter_count = 1;     
-    while(i + 1 < len && ((temp == input[i+1])|| isdigit(input[i+1])|| isspace(input[i+1])))
-    {
-      if(temp == input[i+1]){
-        letter_count++;
-      }
-      if(isdigit(input[i])){
-        printf("Warning digit found: %c\n",input[i]);
-      }
+if(stat(fileName, &st)==0){
+  fileSize = st.st_size;
 
-      i++;
-    }  
-    // Store letter_count in a character array count[] 
-    sprintf(count, "%d", letter_count);
-    //printf("%d\n",letter_count);
- 
-    //Copy the count[] to outputination 
-   for(k = 0; *(count+k); k++, j++)
-   { 
-      if(count[k]== '2'){
-        output[j] = temp;
+}else{
+  printf("file opening error\n");
+  exit(1);
+}
 
-      } 
-      //i can't figure it out atm
-      /*else if (count[k] =='1'){
-        continue;
-      }*/
-      else{
-        output[j]=count[k];
-      }
-    } 
+//error for if asking to create more workers than bytes in the files
+//ask for smaller input , exit(1) the program
+if(numWorkers>fileSize){
+  printf("ERROR: Number of output files is greater than size of file.\n");
+  printf("Please enter a number less than or equal to %d\n", fileSize);
+  exit(1);
+}
 
-     output[j++] = input[i];
+char* parameters[] = {"./worker", argv[1], argv[2], numWorkers, NULL};
+execvp(parameters[0], parameters);
 
-  }//end of if(isalpha())
-
-    if(isspace(input[i])){
-      continue;
-    }
-    if(isdigit(input[i])){
-      printf("warning digit found: %c\n", input[i] );
-      continue;
-    }
-
- }
-
-  output[j] = '\0';
-  return output;
-}   
-
-int main(/*int argc, char * argv[]*/){
-
-/*	if(argc != 2){
-		printf("Error: Invalid number of arguments.\n");
-		exit(1);
-	}*/
-
-/* I can't figure this out rn but its suppose to return a struct with the file size that 
-we can use to divide the work up
-	DIR *dirp;
-	struct dirent *dp;
-
-	if(dirp =opendir(argv[1] ==NULL)){
-		perror("ERROR: file not valid.\n");
-		exit(1);
-	}
-*/
-
-/*testing;*/
-	char str[] = "bb  bjk4kkll";
-	char *comp = compress(str);
-	printf("%s\n", comp);
+//all the inputs good
+//forking everything and execute the worker file
+return 0;
 
 }
